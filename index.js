10e6118d21e459577af3aaa2efa7fee4220be511
Fahima-anny <fahima.info03@gmail.com>
2025-01-04 05:22:50 +0600

    cart collection created

--- index.js
+++ index.js
@@ -34,14 +34,12 @@ async function run() {
 
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("carts");
 
+
+    // all menu here 
     app.get("/menu", async (req, res) => {
         const result = await menuCollection.find().toArray() ;
-        res.send(result) ;
-    })
-
-    app.get("/reviews", async (req, res) => {
-        const result = await reviewCollection.find().toArray() ;
         res.send(result) ;
     })
 
@@ -49,6 +47,26 @@ async function run() {
         const count = await menuCollection.estimatedDocumentCount() ;
         res.send({count}) ;
     })
+
+
+    // all reviews here 
+    app.get("/reviews", async (req, res) => {
+        const result = await reviewCollection.find().toArray() ;
+        res.send(result) ;
+    })
+
+
+    // all carts here 
+  app.post("/carts", async(req, res) => {
+    const query = req.body ;
+    const result = await cartCollection.insertOne(query) ;
+    res.send(result) ;
+  })
+
+  app.get("/carts", async(req, res) => {
+    const result = await cartCollection.find().toArray() ;
+    res.send(result) ;
+  })
 
 
     // Send a ping to confirm a successful connection
